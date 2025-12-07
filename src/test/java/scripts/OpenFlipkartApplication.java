2b8d3f8aabb76d112439ca329f75e5fb524168d8
Pravin <pravinkrjha07@gmail.com>
2025-12-07 18:24:34 +0530

    Testing

---
+++ src/test/java/scripts/OpenFlipkartApplication.java
@@ -0,0 +1,25 @@
+package scripts;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenFlipkartApplication {
+
+	public static void main(String[] args) {
+
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://www.flipkart.com/");
+		
+		driver.close();
+		
+		
+		
+	}
+
+}
